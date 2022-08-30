8d05f46e8de44b5f09651da0189e2f86347376d4
alx <ventas@alectrico.cl>
2022-08-30 07:49:31 -0400

    El deployment del contrato da errro de Deprecation

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,18 @@
-FROM python:3.7.13
+#FROM python:3.7.13
+ARG PGUID=1000
+ARG PUID=1000
+
+FROM python:latest
+
 RUN python -m pip install requests
 RUN python -m pip install --upgrade pip
 RUN pip install eth-tester 
 RUN pip install web3
 RUN pip install py-solc-x
 #RUN pip install -U "web3[tester]"
+COPY ./eth.py ./home/eth.py
+RUN chmod +x ./home/eth.py
+#RUN ./home/eth.py
 WORKDIR ./home
 
 
--- eth.py
+++ eth.py
@@ -57,3 +57,22 @@ print( abi )
 print( "#w3")
 w3 = Web3(Web3.EthereumTesterProvider())
 print( w3) 
+
+print( "# set pre-funded account as sender ")
+w3.eth.default_account = w3.eth.accounts[0]
+
+Greeter = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+print( "# Submit the transaction that deploys the contract")
+tx_hash = Greeter.constructor().transact()
+
+print( "# Wait for the transaction to be mined, and get the transaction receipt" )
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+greeter = w3.eth.contract( address=tx_receipt.contractAddress, abi=abi )
+
+greeter.functions.greet().call() 
+
+tx_hash     = greeter.functions.setGreeting('Nihao').transact()
+tx_receipt  = w3.eth.wait_for_transaction_receipt(tx_hash)
+greeter.functions.greet().call() 
